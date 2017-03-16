f9546eafaf77cb50dac402fb20f74d69055dcfd4
sarahkbarrett <bar15091@byui.edu>
2017-03-16 17:01:10 -0600

    current location assessment - complete

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -23,6 +23,43 @@ $(function () {
   // Get the data from the wunderground API
   function getData(lat, long){
     $.ajax({
+     
+            url : "http://api.wunderground.com/api/3d771b03d1aed7e0/geolookup/conditions/q/autoip.json", 
+            dataType : "jsonp",
+            success : function(data) {
+				console.log(data);
+				
+				var city = data['location']['city'];
+				var state = data['location']['state'];
+				var temp = data['current_observation']['temp_f'];
+				var sum = data["current_observation"]['weather'];
+				var feel = data["current_observation"]["feelslike_f"];
+				var wind = data["current_observation"]["wind_string"];
+				var humid = data["current_observation"]["relative_humidity"]
+				var time = data["current_observation"]["observation_time"];
+				
+		       var cur_location = $('#cityDisplay');
+				var title_location = $('#location_home');
+				var cur_temp = $('#currentTemp');
+				var summary = $("#summary");
+				var feelslike = $("#add1");
+				var windstring = $("#add2");
+				var humidity = $("#add3");
+				var update = $('small');
+				
+       
+        		cur_location.text(city + ", " + state);
+				title_location.text(city + ", " + state + " |  " + "Weather Home");
+				cur_temp.text(temp + "°");
+				summary.text(sum);
+				feelslike.text("Feels like: " + feel + "° F");
+				windstring.text("Wind: " + wind);
+				humidity.text("Humidity: " + humid);
+				update.text(time);
+				
+            }
+            });
+     }
 
 
 
@@ -31,13 +68,13 @@ $(function () {
 
 
       $("#cover").fadeOut(250);
-    }
-           });
+    });
+          
 
-  }
+  
 
   // A function for changing a string to TitleCase
   function toTitleCase(str){
     return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
   }
-});
+
