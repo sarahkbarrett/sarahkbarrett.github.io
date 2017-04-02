ec1505ea6f96692b05c79a35f302d06f7374366f
sarahkbarrett <bar15091@byui.edu>
2017-04-02 14:23:47 -0600

    creation of all webpage files

---
+++ final/classic.js
@@ -0,0 +1,18 @@
+console.log("f")
+$(function(){
+$.getJSON( "classic.json", function( data ) {
+    console.log("test");
+    console.log(data);
+  var items = [];
+  $.each( data, function( key, val ) {
+      console.log("Hello")
+      items.push( "<li><a href='" + val["link"] + "'><img src='" + val["imgSrc"] + "' class='comfy'/></a></li>" );
+      
+  });
+ 
+  $( "<ul/>", {
+    "id": "example",
+    html: items.join( "" )
+  }).appendTo( "main" );
+});
+});
